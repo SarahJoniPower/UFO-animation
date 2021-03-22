05beceae00c279dc9d37e1f84139e9467222011a
Sarah Power <sarahjonipower0@gmail.com>
2021-03-22 22:40:10 +0000

    Removed test circle

--- src/app.jsx
+++ src/app.jsx
@@ -3,8 +3,7 @@ import { UFO } from "./ufo"
 import { Circle } from "./circle"
 export const App = () => {
     return <div>
-        <h1> hey Theo, look I'm using props now: </h1>
-        <Circle />
+        <h1> IT'S MOVING! </h1>
         <UFO />
         {/* <UFO xAxis={300} yAxis={350} /> */}
     </div>
