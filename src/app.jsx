9b7c6e5d8387dea7e6c0105dc5f09607845fa58f
Sarah Power <sarahjonipower0@gmail.com>
2021-03-25 00:22:13 +0000

    css editing

--- src/app.jsx
+++ src/app.jsx
@@ -3,11 +3,11 @@ import { UFO } from "./ufo"
 import "./app.css"
 
 export const App = () => {
-    return <div>
+    return <body>
         <h2> Land a UFO! </h2>
         <h3> Click in the box where you want to land </h3>
         <h4> Orange </h4>
         <UFO xAxis={205} yAxis={265} />
-    </div>
+    </body>
 }
 
