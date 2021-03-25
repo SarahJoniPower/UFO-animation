9f2618af268f5a2d8521b291029daa90416374a0
Sarah Power <sarahjonipower0@gmail.com>
2021-03-25 12:39:54 +0000

    Adding instructions

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,7 @@ import "./app.css"
 export const App = () => {
     return <body>
         <h2> Land a UFO! </h2>
-        <h3> Click in the box where you want to land </h3>
+        <h3> Click in the box where you want to land, we'll slow down on landing!! </h3>
         <h4> Orange </h4>
         <UFO xAxis={125} yAxis={215} speed={5} />
     </body>
